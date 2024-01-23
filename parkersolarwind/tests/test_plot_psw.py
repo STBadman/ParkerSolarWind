b4975e446df7682c54733066ffe0d750f34c871c
Samuel Badman <samuel.badman@cfa.harvard.edu>
2024-01-23 15:57:30 -0500

    Add dummy test suite

---
+++ parkersolarwind/tests/test_plot_psw.py
@@ -0,0 +1,25 @@
+import parkersolarwind as psw
+import plot_parkersolarwind as parkersolarwind
+
+def test_plot_isothermal() :
+    # Make example isothermal solution
+    # Check input error handling
+    # Check fexts error handling
+    # Check output is fig and axes object (3 columns)
+    assert False
+
+def test_plot_polytropic() :
+    # Make example polytropic solution
+    # Check input error handling
+    # Check add_iso handling
+    # Check fexts error handling
+    # Check output is fig and axes object (3 columns)
+    assert False
+
+def test_plot_isothermal_layer() : 
+    # Make example isothermal layer solution
+    # Check input error handling
+    # Check add_iso handling
+    # Check fexts error handling
+    # Check output is fig and axes object (3 columns)
+    assert False
---
+++ parkersolarwind/tests/test_psw.py
@@ -0,0 +1,119 @@
+import parkersolarwind as psw
+
+def test_coronal_base_altitude() :
+    # Check output is a distance
+    assert False
+
+def test_critical_speed() :
+    # Check output is a velocity
+    assert False
+
+def test_critical_radius() :
+    # Check output is a distance
+    assert False
+
+def test_critical_radius_fext() :
+    # Check output is a distance
+    # Check error handling for fext function
+    assert False
+
+def test_ug() :
+    # Check output is a velocity
+    assert False
+
+def test_uc0() : 
+    # Check output is a velocity
+    assert False
+
+def test_scrit() :
+    # Check output is dimensionless
+    # check output shape matches input shape
+    assert False
+
+def test_scrit_fext() :
+    # Check output is dimensionless
+    # check output shape matches input shape
+    # Check error handling for fext function
+    assert False
+
+def test_scrit_solution() :
+    # Check output is 2-array, dimensionless
+    # Check error handling for fext function
+    assert False
+
+def test_scrit_fext_solution() :
+    # Check output is a 2-array, dimensionless
+    # Check error handling for fext function
+    assert False
+
+def test_u0() :
+    # Check output is velocity
+    assert False
+
+def test_u0_fext() :
+    # Check output is velocity
+    # Check error handling for fext function
+    assert False
+
+def test_uc_polytropic() :
+    # Check output is velocity
+    assert False
+
+def test_uc_polytropic_fext() :
+    # Check output is velocity
+    # Check error handling for fext function
+    assert False
+
+def test_parker_isothermal_algebraic() :
+    # Check output is dimensionless
+    assert False
+
+def test_parker_isothermal_fext_algebraic() :
+    # Check output is dimensionless
+    # Check fext error handling
+    assert False
+
+def test_parker_polytropic_algebraic() :
+    # Check output is (km/s)^2 or equivalent
+    assert False
+
+def test_parker_polytropic_fext_algebraic() :
+    # Check output is (km/s)^2 or equivalent
+    # Check fext error handling
+    assert False
+
+def test_isothermal_solution() :
+    # Check input error handling
+    # Check outputs 4 items
+    # Check units of outputs
+    assert False
+
+def test_isothermal_fext_solution() :
+    # Check input error handling including fext and ifext
+    # Check outputs 4 items
+    # Check units of outputs
+    assert False
+
+def test_polytropic_solution() :
+    # Check input error handling
+    # Check outputs 9 items
+    # Check units of outputs
+    assert False
+
+def test_polytropic_fext_solution() :
+    # Check input error handling including fext and ifext
+    # Check outputs 9 items
+    # Check units of outputs
+    assert False
+
+def test_isothermal_layer_solution() : 
+    # Check input error handling
+    # Check outputs 10 items
+    # Check units of outputs
+    assert False
+
+def test_isothermal_layer_fext_solution() : 
+    # Check input error handling including fext and ifext
+    # Check outputs 10 items
+    # Check units of outputs
+    assert False
