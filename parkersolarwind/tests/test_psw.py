1a0a35d79735d8d87c7f8c6d07524fd51087124d
Samuel Badman <samuel.badman@cfa.harvard.edu>
2024-01-25 09:51:48 -0500

    Add tests folder and WIP test suite

--- parkersolarwind/tests/test_psw.py
+++ parkersolarwind/tests/test_psw.py
@@ -1,40 +1,75 @@
 import parkersolarwind as psw
+import astropy.units as u
+import astropy.constants as const
+import numpy as np
+import pytest
+
+def fext_success(r) : 
+    '''
+    To work correctly, a force function must take an array of distances
+    and return an array of the same shape with units equivalent to
+    GM_sun/R_sun^2
+    '''
+    return (const.G*const.M_sun/const.R_sun**2)*np.ones(len(np.array(r)))
+
+def fext_fail(r) : return None
+
+def ifext_success(r1,r2) : 
+    '''
+    To work correctly, an integrated force function must take an 
+    array of inner and outer distances (integration limits) and 
+    return an array of floats the same shape with units of (km/s)^2 
+    (i.e. output should not be astropy.units.quantity.Quantity)
+    '''
+    return (const.G*const.M_sun/const.R_sun).to("km^2/s^2").value*np.ones(len(np.array(r2)))
+
+def ifext_fail(r1,r2) : return None
 
 def test_coronal_base_altitude() :
-    # Check output is a distance
-    assert False
+    # Check default output is a distance
+    assert psw.coronal_base_altitude().unit.is_equivalent(u.m), "must return a distance"
 
 def test_critical_speed() :
     # Check output is a velocity
-    assert False
+    assert psw.critical_speed().unit.is_equivalent(u.m/u.s), "must return a velocity"
 
 def test_critical_radius() :
     # Check output is a distance
-    assert False
+    assert psw.critical_radius().unit.is_equivalent(u.m), "must_return a distance"
 
-def test_critical_radius_fext() :
+def test_critical_radius_fext(fext_success,fext_fail) :
     # Check output is a distance
     # Check error handling for fext function
+
     assert False
 
 def test_ug() :
     # Check output is a velocity
-    assert False
+    assert psw.get_ug(1*u.R_sun).unit.is_equivalent(u.m/u.s), "must return velocity"
 
 def test_uc0() : 
     # Check output is a velocity
-    assert False
+    assert psw.get_uc0(5/3,1*u.MK).unit.is_equivalent(u.m/u.s), "must return velocity"
 
 def test_scrit() :
     # Check output is dimensionless
+    assert type(psw.s_crit(1,1*u.MK,5/3)) is not u.quantity.Quantity, "must return dimensionless"
     # check output shape matches input shape
-    assert False
+    array_test = np.ones(10)
+    assert psw.s_crit(array_test,1*u.MK,5/3).shape == array_test.shape, "output shape must match input shape"
 
 def test_scrit_fext() :
+    array_test = np.ones(10)
+    out = psw.s_crit_fext(array_test,1*u.MK,5/3,
+                          fext_success,ifext_success
+                        )
     # Check output is dimensionless
+    assert out.unit.is_equivalent(u.m/u.m), "must return dimensionless"
     # check output shape matches input shape
+    assert out.shape == array_test.shape
     # Check error handling for fext function
-    assert False
+    with pytest.raises(AssertionError) :
+        psw.s_crit_fext(array_test,1*u.MK,5/3,[],[])
 
 def test_scrit_solution() :
     # Check output is 2-array, dimensionless
